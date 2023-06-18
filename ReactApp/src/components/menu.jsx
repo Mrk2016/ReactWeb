8a47b058afcba0694ab2e62bff0ace2cbaa1ce6b
Marcos Rubi <markrubi2@gmail.com>
2023-06-18 01:29:05 -0600

    actualizacion

---
+++ ReactApp/src/components/menu.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const menu = () => (
+  <nav>
+    <ul>
+      <li>hola</li>
+    </ul>
+  </nav>
+);
+
+export default menu;
---
+++ ReactApp/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx}", './components/**/*.{html,js}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}

071f845ea278abc31144ae5dc6b78fa76261aac4
Marcos Rubi <markrubi2@gmail.com>
2023-06-18 10:48:36 -0600

    progress

--- ReactApp/src/components/menu.jsx
+++ ReactApp/src/components/menu.jsx
@@ -1,9 +1,20 @@
 import React from "react";
+import { navLinks } from "../constants";
 
 const menu = () => (
-  <nav>
-    <ul>
-      <li>hola</li>
+  <nav className="w-full flex py-6 justify-between items-center nabvar">
+    <img src="src/assets/react.svg" className="" alt="" />
+    <ul className="list-none sm:flex hidden justify-end items-center flex-1">
+      {navLinks.map((nav, index) => (
+        <li
+          key={nav.id}
+          className={`font-nomal cursor-pointer text-[16px] ${
+            index === navLinks.length - 1 ? "mr-0" : "mr-10"
+          }`}
+        >
+          <a href={`#${nav.id}`}>{nav.title}</a>
+        </li>
+      ))}
     </ul>
   </nav>
 );
